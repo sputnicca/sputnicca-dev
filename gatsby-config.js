40227af19513c10d100044114854f1a31adf3340
sputnicca <ananas27x@gmail.com>
2022-04-05 19:40:46 +0900

    gh-pagesによるデプロイ設定追加

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,18 +4,11 @@ const shouldAnalyseBundle = process.env.ANALYSE_BUNDLE
 
 module.exports = {
   siteMetadata: {
-    // You can overwrite values here that are used for the SEO component
-    // You can also add new values here to query them like usual
-    // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-minimal-blog/gatsby-config.js
-    siteTitle: `Minimal Blog`,
-    siteTitleAlt: `Minimal Blog - Gatsby Theme`,
-    siteHeadline: `Minimal Blog - Gatsby Theme from @lekoarts`,
-    siteUrl: `https://minimal-blog.lekoarts.de`,
-    siteDescription: `Typography driven, feature-rich blogging theme with minimal aesthetics. Includes tags/categories support and extensive features for code blocks such as live preview, line numbers, and line highlighting.`,
-    siteLanguage: `en`,
-    siteImage: `/banner.jpg`,
-    author: `@lekoarts_de`,
+    title: `sputnicca-dev`,
+    author: `@sputnicca`,
+    siteUrl: `https://sputnicca.github.io/sputnicca-dev`,
   },
+  pathPrefix: "/sputnicca-dev",
   plugins: [
     {
       resolve: `@lekoarts/gatsby-theme-minimal-blog`,
